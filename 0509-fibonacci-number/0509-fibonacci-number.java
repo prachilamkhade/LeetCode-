83257146c727bb9a7566c50a2edfeaa7a1d92aa5
Prachi Lamkhade <lamkhadeprachi12@gmail.com>
2026-08-23 23:13:40 +0530

    Time: 10 ms (26.51%), Space: 41.6 MB (98.32%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        int sum=0;
+        if(n==0)
+            return 0;
+
+        if(n==1)
+            return 1;
+
+        sum=fib(n-2)+fib(n-1);
+    
+        return sum;
+        
+    }
+}
